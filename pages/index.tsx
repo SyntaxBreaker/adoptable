877aab1ef9fd2432e2b9e71eab928c6ebebcceb1
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-01-02 14:26:52 +0100

    Add homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -10,8 +11,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>Hello world!</h1>
+      <main className={styles['homepage']}>
+       <h2 className={styles['homepage__title']}>Find Your New Best Friend</h2>
+       <form className={styles['homepage__form']}>
+          <input name='zipcode' id='zipcode' type='number' placeholder='Enter your zipcode to search Available Pets' className={styles['homepage__input']} />
+          <input type='submit' value='Find my new pet' className={`${styles['homepage__input']} ${styles['homepage__input--submit']}`} />
+       </form>
       </main>
     </>
   )
