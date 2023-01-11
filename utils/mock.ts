37cb7aec55be1a3598f6e197d585becc9f2def4e
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-01-11 15:46:43 +0100

    Add pets mock

---
+++ utils/mock.ts
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 202,
+        name: 'name1',
+        type: 'Bird',
+        breed: 'Breed', 
+        gender: 'Female',
+        age: 'Baby',
+        size: 'Small',
+        location: 'Somewhere 22'
+    },
+    {
+        id: 203,
+        name: 'name2',
+        type: 'Bird',
+        breed: 'Breed', 
+        gender: 'Male',
+        age: 'Baby',
+        size: 'Small',
+        location: 'Somewhere 22'
+    },
+    {
+        id: 204,
+        name: 'name3',
+        type: 'Bird',
+        breed: 'Breed', 
+        gender: 'Female',
+        age: 'Baby',
+        size: 'Small',
+        location: 'Somewhere 22'
+    },
+    {
+        id: 205,
+        name: 'name4',
+        type: 'Bird',
+        breed: 'Breed', 
+        gender: 'Male',
+        age: 'Baby',
+        size: 'Small',
+        location: 'Somewhere 22'
+    },
+    {
+        id: 206,
+        name: 'name5',
+        type: 'Bird',
+        breed: 'Breed', 
+        gender: 'Male',
+        age: 'Baby',
+        size: 'Small',
+        location: 'Somewhere 22'
+    },
+    {
+        id: 207,
+        name: 'name6',
+        type: 'Bird',
+        breed: 'Breed', 
+        gender: 'Female',
+        age: 'Baby',
+        size: 'Small',
+        location: 'Somewhere 22'
+    },
+]
