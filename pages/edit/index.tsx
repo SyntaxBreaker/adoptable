85739f476d0e1cd577bd07c2d2aa42ebe7b27a04
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-01-30 15:32:24 +0100

    Add a page for editing pet announcements

---
+++ pages/edit/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Form from "../../components/Form";
+
+function Edit() {
+    return (
+        <>
+            <Head>
+                <title>Edit pet announcement</title>
+            </Head>
+            <Form />
+        </>
+    )
+}
+
+export default Edit;
