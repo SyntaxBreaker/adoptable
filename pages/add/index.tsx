a228b0b95111eb7dca62fc92718b701966975120
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-01-30 14:55:03 +0100

    Create a page for adding a pet

---
+++ pages/add/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Form from '../../components/Form';
+
+function Add() {
+    return (
+        <>
+            <Head>
+                <title>Add a pet</title>
+            </Head>
+            <Form />
+        </>
+    )
+}
+
+export default Add;
