39ac822c702318e43e5053780a81e92fc879e034
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-02-02 17:30:06 +0100

    Implement Auth0

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,19 @@
 import styles from '../../styles/Header.module.scss';
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Header() {
+    const { user, error, isLoading } = useUser();
+
     return (
         <header className={styles['header']}>
             <Link href='/' className={styles['header__title']}>Adoptable</Link>
-            <Link href="/account" className={styles['header__link']}>Account</Link>
+            {user ? (
+                <div className={styles['header__links']}>
+                    <Link href="/account" className={styles['header__link']}>Account</Link>
+                    <Link href="/api/auth/logout" className={styles['header__link']}>Logut</Link>
+                </div>
+            ) : <Link href="/api/auth/login" className={styles['header__link']}>Login</Link>}
         </header>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <Header />
-    <Component {...pageProps} />
-  </>
+    <UserProvider>
+      <Header />
+      <Component {...pageProps} />
+    </UserProvider>
   )
 }
