cb17cf34e029ddef4245bda1fab10572ab484416
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-03-03 23:49:43 +0100

    Remove the unnecessary bookmark button from the card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -4,7 +4,6 @@ import IPet from '../../types/pet';
 export default function Card(pet: IPet) {
     return (
         <div className={styles['pet-card']} style={{ backgroundImage: `url(${pet.images[0]})` }}>
-            <button className={styles['pet-card__button']}>💙</button>
             <div className={styles['pet-card__body']}>
                 <p className={styles['pet-card__name']}>{pet.name}</p>
                 <p className={styles['pet-card__gender']}>{pet.gender}</p>
