03f235f538adea2b5618b69dd8d26b5e140c054d
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-03-05 17:12:06 +0100

    Fix the layout of the pet page

--- pages/pet/[index].tsx
+++ pages/pet/[index].tsx
@@ -21,14 +21,14 @@ function Pet(pet: IPet) {
             <div className={styles['pet__header']}>
                 <Image src={pet.images[0]} width={600} height={600} alt='pet image' className={styles['pet__image']} />
             </div>
-            {user && user.email === pet.authorId &&
-                <div className={styles['pet__buttons']}>
-                    {!isExist ? <button onClick={() => saveToLocalStorage('favorites', pet.id, setIsExist)} className={styles['pet__button']}>Bookmark</button> : <button onClick={() => removeFromLocalStorage('favorites', pet.id, setIsExist)} className={styles['pet__button']}>Unbookmark</button>}
-                    <Link href={`/edit/${pet.id}`} className={styles['pet__button']}>Edit</Link>
-                    <button className={styles['pet__button']}>Remove</button>
-                </div>
-            }
             <div className={styles['pet__body']}>
+                {user && user.email === pet.authorId &&
+                    <div className={styles['pet__buttons']}>
+                        {!isExist ? <button onClick={() => saveToLocalStorage('favorites', pet.id, setIsExist)} className={styles['pet__button']}>Bookmark</button> : <button onClick={() => removeFromLocalStorage('favorites', pet.id, setIsExist)} className={styles['pet__button']}>Unbookmark</button>}
+                        <Link href={`/edit/${pet.id}`} className={styles['pet__button']}>Edit</Link>
+                        <button className={styles['pet__button']}>Remove</button>
+                    </div>
+                }
                 <h2 className={styles["pet__heading"]}>Adoptable pets.</h2>
                 <p>I&apos;m looking for my new home.</p>
                 <p>Could you be my new partner?</p>
