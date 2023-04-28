1f7110db9f85fad8eae758185e3b378457abe007
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-04-28 14:17:42 +0200

    Implement useClickOutsideElement in the header navigation

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from '../../styles/Header.module.scss';
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import useClickOutsideElement from '../../hooks/useClickOutsideElement';
 
 export default function Header() {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-
+    const navRef = useRef(null);
+    const buttonRef = useRef(null);
     const { user, error, isLoading } = useUser();
+
+    useClickOutsideElement(navRef, buttonRef, setIsOpen);
 
     return (
         <header className={styles['header']}>
             <Link href='/' className={styles['header__title']}>Adoptable</Link>
-            <button className={styles['header__button']} onClick={() => setIsOpen(!isOpen)}>&#9776;</button>
-            <nav className={`${styles['header__menu']} ${isOpen && styles['header__menu--active']}`} onClick={() => setIsOpen(!isOpen)}>
+            <button className={styles['header__button']} onClick={() => setIsOpen(!isOpen)} ref={buttonRef}>&#9776;</button>
+            <nav className={`${styles['header__menu']} ${isOpen && styles['header__menu--active']}`} onClick={() => setIsOpen(!isOpen)} ref={navRef}>
                 <Link href="/favorites" className={styles['header__link']}>Favorites</Link>
                 {user ? (
                     <>
