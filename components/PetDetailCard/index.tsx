385f60d40e39c5e742a8df2006bc6fd0d9a4285a
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-10-11 16:40:01 +0200

    Improve PetDetailCard appearance

--- components/PetDetailCard/index.tsx
+++ components/PetDetailCard/index.tsx
@@ -1,14 +1,10 @@
 import styles from '../../styles/Pet.module.scss';
-import Image from 'next/image';
 
 function PetDetailCard({ name, value }: { name: string, value: string | number }) {
     return (
         <div className={styles["pet__card"]}>
-            <Image src='/pet.svg' width={48} height={48} alt='' />
-            <div className={styles["pet__description"]}>
-                <p className={styles["pet__title"]}>{name}</p>
-                <p className={styles["pet__text"]}>{value}</p>
-            </div>
+            <p className={styles["pet__title"]}>{name}:</p>
+            <p className={styles["pet__text"]}>{value}</p>
         </div>
     )
 }
