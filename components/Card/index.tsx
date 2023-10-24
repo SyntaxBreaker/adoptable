e51c87ab38043126a30a654a2f3fd0f5dfe63de3
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-10-24 11:25:47 +0200

    Update card style

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -6,7 +6,11 @@ export default function Card(pet: IPet) {
         <div className={styles['pet-card']} style={{ backgroundImage: `url(${pet.images.length > 0 ? pet.images[0] : '/default.png'})` }}>
             <div className={styles['pet-card__body']}>
                 <p className={styles['pet-card__name']}>{pet.name}</p>
-                <p className={styles['pet-card__gender']}>{pet.gender}</p>
+                <div className={styles['pet-card__information']}>
+                    <p className={styles['pet-card__gender']}>{pet.gender}</p>
+                    <span>|</span>
+                    <p className={styles['pet-card__species']}>{pet.species}</p>
+                </div>
             </div>
         </div>
     )
