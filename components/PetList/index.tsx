e662bc63f35ee121486edf2dbf8d9a449d68bec2
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-10-25 10:42:42 +0200

    Improve empty state layout in PetList component

--- components/PetList/index.tsx
+++ components/PetList/index.tsx
@@ -5,6 +5,9 @@ import filterData from '../../utils/filter';
 import IPet from '../../types/pet';
 import Link from "next/link";
 import Card from "../Card";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import { usePathname } from 'next/navigation';
 
 function PetList({ pets }: { pets: IPet[] }) {
     const [filters, setFilters] = useState(filterData);
@@ -12,6 +15,9 @@ function PetList({ pets }: { pets: IPet[] }) {
     const [gender, setGender] = useState<string[]>([]);
     const [age, setAge] = useState<string[]>([]);
     const [size, setSize] = useState<string[]>([]);
+
+    const { user, error, isLoading } = useUser();
+    const pathname = usePathname();
 
     const filteredPets = species?.length || gender?.length || age?.length || size?.length
         ? pets.filter(pet => {
@@ -67,13 +73,23 @@ function PetList({ pets }: { pets: IPet[] }) {
             <Filter filters={filters} setFilters={setFilters} />
             <div className={styles['pets__container']}>
                 <h2>{filteredPets.length} results in pets!</h2>
-                <div className={styles['pets__wrapper']}>
+                {filteredPets.length > 0 ? <div className={styles['pets__wrapper']}>
                     {filteredPets && filteredPets.map(pet => (
                         <Link href={`/pet/${pet.id}`} key={pet.id} className={styles['pets__link']}>
                             <Card {...pet} />
                         </Link>
                     ))}
-                </div>
+                </div> : <div className={styles['pets__empty']}>
+                    <Image src="./empty.svg" alt="" width={300} height={300} />
+                    <h3 className={styles['pets__heading']}>It&apos;s empty here!</h3>
+                    <p className={styles['pets__text']}>{pathname === '/pets' ? 'No pets are available.' : 'No favorite pets are available.'}</p>
+                    {user && (
+                        <>
+                            <p className={styles['pets__suggestion']}>Why not add some?</p>
+                            <Link href={`${pathname === '/pets' ? '/add' : '/pets'}`} className={styles['pets__button']}>{pathname === '/pets' ? 'Add pet' : 'See all pets'}</Link>
+                        </>
+                    )}
+                </div>}
             </div>
         </div>
     )
