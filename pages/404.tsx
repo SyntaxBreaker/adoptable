232ac6a72b2c6fa6e19713a8c6feea6f7ddcfc39
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-10-30 10:58:04 +0100

    Improve 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import styles from '../styles/404.module.scss';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -6,11 +7,16 @@ function Custom404() {
     const router = useRouter();
 
     return (
-        <div className={styles['error']}>
-            <Image src="404.svg" width={350} height={350} alt='' />
-            <h2 className={styles['error__heading']}>Oops! Page Not Found!</h2>
-            <button className={styles['error__button']} onClick={() => router.push('/')}>Go Back</button>
-        </div>
+        <>
+            <Head>
+                <title>404 page - Adoptable</title>
+            </Head>
+            <div className={styles['error']}>
+                <Image src="404.svg" width={350} height={350} alt='' />
+                <h2 className={styles['error__heading']}>Oops! Page Not Found!</h2>
+                <button className={styles['error__button']} onClick={() => router.push('/')}>Go Back</button>
+            </div>
+        </>
     )
 }
 
