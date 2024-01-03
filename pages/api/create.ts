8985ab131aeb9c96148362db06f4cf220fbd8dbd
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-01-03 13:17:36 +0100

    Improve HTTP status code for pet addition

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         const data = JSON.parse(req.body);
         await prisma.pets.create({ data });
-        res.status(200).json('The pet was added.');
+        res.status(201).json('The pet was added.');
     } catch (err) {
         res.status(500).json(`An error occurred.`);
     }
